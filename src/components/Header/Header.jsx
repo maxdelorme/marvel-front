bfaf14570983db63b6bbce5949ad4e266a541c13
Maxime Delorme <maxime@familledelorme.com>
2025-11-29 20:19:16 +0100

    add button to disconnect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,26 @@ import logo from "@/assets/img/logo.png";
 import "./header.css";
 import { Link, NavLink } from "react-router-dom";
 import { IoHeart } from "react-icons/io5";
+import axios from "axios";
+import { backURL } from "../../utils/settings";
 
-const Header = () => {
+const Header = ({ token, setToken }) => {
+  const disconnect = async () => {
+    try {
+      await axios.get(backURL + "/user/disconnect", {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      setToken(null);
+    } catch (error) {
+      console.log(
+        "error",
+        error.reponse ? error.response.data.message : error.message
+      );
+      // even if not authenticated, we clean all on browser
+      setToken(null);
+    }
+  };
+
   return (
     <header>
       <div className="container">
@@ -16,6 +34,7 @@ const Header = () => {
           <NavLink to="/favorites">
             <IoHeart /> Favorites
           </NavLink>
+          {token && <button onClick={disconnect}>Se déconnecter</button>}
         </nav>
       </div>
     </header>
