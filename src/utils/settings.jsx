e2e37ad112339658a33477d15169f1c8ec6eed2b
Maxime Delorme <maxime@familledelorme.com>
2025-11-30 13:50:36 +0100

    change to remote url

--- src/utils/settings.jsx
+++ src/utils/settings.jsx
@@ -1,2 +1,2 @@
-// export const backURL = "https://site--marvel-back--5sgz5mzbgxzv.code.run";
-export const backURL = "http://localhost:3000";
+export const backURL = "https://site--marvel-back--5sgz5mzbgxzv.code.run";
+// export const backURL = "http://localhost:3000";
